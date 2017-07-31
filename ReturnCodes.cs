201edd1a9a52c98b22eab6a4ea579b0aa9c14d15
marcus <marcus@thehellstens.com>
2017-07-30 22:37:33 -0400

    Fix gap in return codes

--- ReturnCodes.cs
+++ ReturnCodes.cs
@@ -7,10 +7,10 @@ namespace FolderCompare
     {
         NoErrors = 0,
         MissingFile = 1,
-        DifferentFile = 1 << 2,
-        NoAccess = 1 << 3,
-        DirectoryNoAccess = 1 << 4,
-        InvalidArgumentFolderDoesNotExist = 1 << 5,
-        InvalidArguments = 1 << 6
+        DifferentFile = 1 << 1,
+        NoAccess = 1 << 2,
+        DirectoryNoAccess = 1 << 3,
+        InvalidArgumentFolderDoesNotExist = 1 << 4,
+        InvalidArguments = 1 << 5
     }
 }
